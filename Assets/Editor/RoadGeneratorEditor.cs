a83bd671f20ec84eb2b17e897f4b87a97d3676fd
IamBartjuuh <bartatgier@hotmail.com>
2026-06-17 11:43:45 +0200

    Procedural pt 2.
    - In de inspector knoppen toegevoegd voor het opnieuw genereren.
    
    Known Issues:
    - T-junction grenst aan elkaar.
    - Veel andere objecten zitten er niet in. (omgeving niet af)

---
+++ Assets/Editor/RoadGeneratorEditor.cs
@@ -0,0 +1,36 @@
+#if UNITY_EDITOR
+using BusBoys.Assets.Scripts.Core.Graph;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ProceduralRoadGraphGenerator))]
+public class RoadGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GUILayout.Space(10);
+
+        ProceduralRoadGraphGenerator generator = (ProceduralRoadGraphGenerator)target;
+
+        if (GUILayout.Button("Generate Roads", GUILayout.Height(30)))
+        {
+            generator.Generate();
+            EditorUtility.SetDirty(generator);
+        }
+
+        if (GUILayout.Button("Reset + New Seed", GUILayout.Height(30)))
+        {
+            generator.NewSeedAndGenerate();
+            EditorUtility.SetDirty(generator);
+        }
+
+        if (GUILayout.Button("Clear Roads", GUILayout.Height(24)))
+        {
+            generator.ClearGenerated();
+            EditorUtility.SetDirty(generator);
+        }
+    }
+}
+#endif
